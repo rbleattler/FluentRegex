a219b7c1bc8cb8338ee0eed7edd0114dea5b8f82
Robert Bleattler <rob.bleattler@viewpointe.com>
2024-05-28 14:37:48 -0400

    Update `LocalBuild.ps1` to be more verbose, and configurable

--- Scripts/LocalBuild.ps1
+++ Scripts/LocalBuild.ps1
@@ -1,10 +1,25 @@
 param(
+  [Parameter(ParameterSetName = 'Package')]
   [string]
-  $Version = '1.0.1'
+  $Version = '0.0.1',
+  [Parameter(ParameterSetName = 'Package')]
+  [switch]
+  $Pack,
+  [Parameter(ParameterSetName = 'Package')]
+  [ValidateSet('Debug', 'Release')]
+  [string]
+  $Configuration = 'Release'
 )
+Write-Host "Cleaning project..." -ForegroundColor Yellow &&
 dotnet clean &&
+Write-Host "Restoring packages..." -ForegroundColor Yellow &&
 dotnet restore &&
+Write-Host "Building version $Version..." -ForegroundColor Yellow &&
 dotnet build &&
+Write-Host "Running tests..." -ForegroundColor Yellow &&
 dotnet test &&
-dotnet publish $PSScriptRoot\..\src\RegExpBuilder\RegExpBuilder.csproj --configuration Release /p:Version="$Version" /p:InformationalVersion="$Version-local" &&
-dotnet pack $PSScriptRoot\..\src\RegExpBuilder\RegExpBuilder.csproj --output $PSScriptRoot\..\scratch -p:NuspecFile=$PSScriptRoot\..\src\RegExpBuilder\RegExpBuilder.nuspec -p:version="$Version" /p:PackageVersion="$Version" /p:PackageReleaseNotes="Local build" /p:PackageTags="local"
+Write-Host "Publishing..." -ForegroundColor Yellow &&
+dotnet publish $PSScriptRoot\..\src\RegExpBuilder\RegExpBuilder.csproj --configuration $Configuration /p:Version="$Version" /p:InformationalVersion="$Version-local" &&
+if ($PSCmdlet.ParameterSetName -eq 'Package' -and $Pack){
+  dotnet pack $PSScriptRoot\..\src\RegExpBuilder\RegExpBuilder.csproj --output $PSScriptRoot\..\scratch -p:NuspecFile=$PSScriptRoot\..\src\RegExpBuilder\RegExpBuilder.nuspec -p:version="$Version" /p:PackageVersion="$Version" /p:PackageReleaseNotes="Local build" /p:PackageTags="local"
+}
