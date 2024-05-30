8abd70250ed588d28523cd611f21dead7e99d11d
Rob Bleattler <rbleattler@outlook.com>
2024-05-30 18:06:49 -0400

    refactor: Improve GroupBuilderTests by organizing test classes and adding missing test cases

--- src/FluentRegex.Tests/GroupBuilder.Tests.cs
+++ src/FluentRegex.Tests/GroupBuilder.Tests.cs
@@ -1,25 +1,38 @@
 using System;
 using System.Text;
 using FluentRegex;
-namespace FluentRegexTests;
+namespace FluentRegexTests.GroupBuilderTests;
 
-public class GroupBuilderTests
+public abstract class GroupBuilderTestGroup
 {
+  protected GroupBuilder _groupBuilder;
+  protected PatternBuilder _patternBuilder;
+  protected string result;
 
-  private GroupBuilder _groupBuilder;
-  private PatternBuilder _patternBuilder;
-  private string result;
-
-  public GroupBuilderTests()
+  public GroupBuilderTestGroup()
   {
     result = "";
     _patternBuilder = new PatternBuilder();
     _groupBuilder = new GroupBuilder(_patternBuilder);
   }
 
+  public void ClearGroupBuilderPattern()
+  {
+    _groupBuilder.Pattern.Clear();
+  }
 
+  public void ClearPatternBuilderPattern()
+  {
+    _patternBuilder.Pattern.Clear();
+  }
+
+}
+
+[Collection("Capture Groups")]
+public class CaptureGroupTests : GroupBuilderTestGroup
+{
   [Fact]
-  public void TestCaptureGroup()
+  public void WhenAppendingLiteral_ShouldReturnExpectedString()
   {
     result = _patternBuilder.CaptureGroup()
                               .AppendLiteral("test")
@@ -27,9 +40,13 @@ public class GroupBuilderTests
                             .ToString();
     Assert.Equal("(test)", result);
   }
+}
 
+[Collection("Non-Capture Groups")]
+public class NonCaptureGroupTests : GroupBuilderTestGroup
+{
   [Fact]
-  public void TestNonCaptureGroup()
+  public void WhenAppendingLiteral_ShouldReturnExpectedString()
   {
     result = _patternBuilder.NonCaptureGroup()
                               .AppendLiteral("test")
@@ -37,17 +54,111 @@ public class GroupBuilderTests
                             .ToString();
     Assert.Equal("(?:test)", result);
   }
+}
 
-  [Fact]
-  public void TestNamedCaptureGroup()
+[Collection("Named Capture Groups")]
+public class NamedCaptureGroupTests : GroupBuilderTestGroup
+{
+  [Fact(DisplayName = "Named Capture Group<Angle Brackets>: When Appending Literal, Should Return Expected String")]
+  public void NamedCaptureGroup_WithAngleBrackets_WhenAppendingLiteral_ShouldReturnExpectedString()
   {
-    // var result = _groupBuilder.NamedCaptureGroup("name", "test", NamedGroupStyle.AngleBrackets);
     result = _patternBuilder.NamedCaptureGroup(NamedGroupStyle.AngleBrackets, "name")
                               .AppendLiteral("test")
                               .Build()
                             .ToString();
     Assert.Equal("(?<name>test)", result);
   }
+
+  [Fact(DisplayName = "Named Capture Group<Single Quote>: When Appending Literal, Should Return Expected String")]
+  public void NamedCaptureGroup_WithSingleQuote_WhenAppendingLiteral_ShouldReturnExpectedString()
+  {
+    result = _patternBuilder.NamedCaptureGroup(NamedGroupStyle.SingleQuote, "name")
+                              .AppendLiteral("test")
+                              .Build()
+                            .ToString();
+    Assert.Equal("(?'name'test)", result);
+  }
+
+  [Fact(DisplayName = "Named Capture Group<P Style>: When Appending Literal, Should Return Expected String")]
+  public void NamedCaptureGroup_WithPStyle_WhenAppendingLiteral_ShouldReturnExpectedString()
+  {
+    result = _patternBuilder.NamedCaptureGroup(NamedGroupStyle.PStyle, "name")
+                              .AppendLiteral("test")
+                              .Build()
+                            .ToString();
+    Assert.Equal("(?P<name>test)", result);
+  }
+}
+
+[Collection("StartGroup Method")]
+public class StartGroupTests : GroupBuilderTestGroup
+{
+  [Fact(DisplayName = "Capture Group: When Starting Group, Should Return Expected String")]
+  public void WhenStartingGroup_ShouldReturnExpectedString()
+  {
+    ClearGroupBuilderPattern();
+    result = _groupBuilder.StartGroup()
+                          .Pattern
+                          .ToString();
+    Assert.Equal("(", result);
+  }
+
+  [Fact(DisplayName = "Non-Capturing Group: When Starting Group, Should Return Expected String")]
+  public void NonCapturingGroup_WhenStartingGroup_ShouldReturnExpectedString()
+  {
+    _groupBuilder = _patternBuilder.NonCaptureGroup();
+    ClearGroupBuilderPattern();
+    result = _groupBuilder
+                  .StartGroup()
+                  .Pattern
+                  .ToString();
+    Assert.Equal("(?:", result);
+
+  }
+
+  [Fact(DisplayName = "Named Capture Group<Angle Brackets>: When Starting Group, Should Return Expected String")]
+  public void NamedCaptureGroup_WithAngleBrackets_WhenStartingGroup_ShouldReturnExpectedString()
+  {
+    _groupBuilder = _patternBuilder.NamedCaptureGroup(NamedGroupStyle.AngleBrackets, "name");
+    ClearGroupBuilderPattern();
+    result = _groupBuilder
+                  .StartGroup()
+                  .Pattern
+                  .ToString();
+    Assert.Equal("(?<name>", result);
+  }
+
+  [Fact(DisplayName = "Named Capture Group<P Style>: When Starting Group, Should Return Expected String")]
+  public void NamedCaptureGroup_WithPStyle_WhenStartingGroup_ShouldReturnExpectedString()
+  {
+    _groupBuilder = _patternBuilder.NamedCaptureGroup(NamedGroupStyle.PStyle, "name");
+    ClearGroupBuilderPattern();
+    result = _groupBuilder
+                  .StartGroup()
+                  .Pattern
+                  .ToString();
+    Assert.Equal("(?P<name>", result);
+  }
+
+  [Fact(DisplayName = "Named Capture Group<Single Quote>: When Starting Group, Should Return Expected String")]
+  public void NamedCaptureGroup_WithSingleQuote_WhenStartingGroup_ShouldReturnExpectedString()
+  {
+    _groupBuilder = _patternBuilder.NamedCaptureGroup(NamedGroupStyle.SingleQuote, "name");
+    ClearGroupBuilderPattern();
+    result = _groupBuilder
+                  .StartGroup()
+                  .Pattern
+                  .ToString();
+    Assert.Equal("(?'name'", result);
+  }
+
+
+
+}
+
+[Collection("NotImplemented")]
+public class NotImplemented : GroupBuilderTestGroup
+{
 
   [Fact(Skip = "Not implemented yet")]
   public void TestAtomicGroup()
@@ -62,29 +173,4 @@ public class GroupBuilderTests
     // var result = _groupBuilder.SubpatternGroupNumberReset("test");
     Assert.Equal("(?|test)", result);
   }
-
-  [Fact]
-  public void TestStartGroup()
-  {
-    Action resetBuilders = () =>
-    {
-      _patternBuilder = new PatternBuilder();
-      _groupBuilder = new GroupBuilder(_patternBuilder);
-    };
-    result = _groupBuilder.StartGroup()
-                          .Pattern
-                          // .AppendLiteral("test")
-                          // .Build()
-                          .ToString();
-    Assert.Equal("(", result);
-    resetBuilders();
-    _groupBuilder = _patternBuilder.NonCaptureGroup();
-    result = _groupBuilder
-                  .StartGroup()
-                  .Pattern
-                  .ToString();
-    Assert.Equal("(?:", result);
-
-  }
-
 }
