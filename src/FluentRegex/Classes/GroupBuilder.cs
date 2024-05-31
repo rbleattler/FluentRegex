6dc35bfd09e8b3df21da5a908b3f76310dc38c96
Rob Bleattler <rbleattler@outlook.com>
2024-05-31 13:57:40 -0400

    refactor: Update GroupBuilder.cs to use Builder instead of PatternBuilder

--- src/FluentRegex/Classes/GroupBuilder.cs
+++ src/FluentRegex/Classes/GroupBuilder.cs
@@ -33,55 +33,55 @@ public class GroupBuilder : Builder
 
   // private new readonly StringBuilder _pattern = new StringBuilder();
 
-  private readonly PatternBuilder _regexBuilder;
-  private readonly NamedGroupStyle? _namedGroupStyle = null;
-  private readonly string? _groupName = null;
-  private readonly GroupType _groupType = GroupType.Capturing;
+  internal Builder _builder;
+  internal NamedGroupStyle? _namedGroupStyle = null;
+  internal string? _groupName = null;
+  internal GroupType _groupType = GroupType.Capturing;
 
   /// <summary>
   /// Initializes a new instance of the <see cref="GroupBuilder"/> class with a <see cref="PatternBuilder"/>, and the declared <see cref="NamedGroupStyle"/> and <see cref="GroupName"/>.
   /// </summary>
-  /// <param name="regexBuilder"></param>
+  /// <param name="patternBuilder"></param>
   /// <param name="namedGroupStyle"></param>
   /// <param name="groupName"></param>
-  public GroupBuilder(PatternBuilder regexBuilder, NamedGroupStyle namedGroupStyle = NamedGroupStyle.AngleBrackets, string? groupName = null)
+  public GroupBuilder(Builder patternBuilder, NamedGroupStyle namedGroupStyle = NamedGroupStyle.AngleBrackets, string? groupName = null)
   {
     // If the group name is null or whitespace, generate a random name.
     if (string.IsNullOrWhiteSpace(groupName))
     {
       groupName = "group" + Guid.NewGuid().ToString().Replace("-", "");
     }
-    _regexBuilder = regexBuilder;
+    _builder = patternBuilder;
     _namedGroupStyle = namedGroupStyle;
     _groupName = groupName;
     _groupType = GroupType.NamedCapturing;
-    StartGroup();
+    Start_Group();
   }
 
   /// <summary>
   /// Initializes a new instance of the <see cref="GroupBuilder"/> class with a <see cref="PatternBuilder"/>, and the declared <see cref="GroupType"/>.
   /// </summary>
-  /// <param name="regexBuilder"></param>
+  /// <param name="patternBuilder"></param>
   /// <param name="groupType"></param>
-  public GroupBuilder(PatternBuilder regexBuilder, GroupType groupType)
+  public GroupBuilder(Builder patternBuilder, GroupType groupType)
   {
     _groupType = groupType;
     if (groupType == GroupType.NamedCapturing)
       throw new ArgumentException("Named capturing groups must have a name.", nameof(groupType));
 
-    _regexBuilder = regexBuilder;
-    StartGroup();
+    _builder = patternBuilder;
+    Start_Group();
   }
 
 
   /// <summary>
   /// Initializes a new instance of the <see cref="GroupBuilder"/> class with a <see cref="PatternBuilder"/>.
   /// </summary>
-  /// <param name="regexBuilder"></param>
-  public GroupBuilder(PatternBuilder regexBuilder)
+  /// <param name="patternBuilder"></param>
+  public GroupBuilder(Builder patternBuilder)
   {
-    _regexBuilder = regexBuilder;
-    StartGroup();
+    _builder = patternBuilder;
+    Start_Group();
   }
 
   /// <summary>
@@ -89,7 +89,7 @@ public class GroupBuilder : Builder
   /// </summary>
   /// <returns> The <see cref="PatternBuilder"/>. </returns>
 
-  public override PatternBuilder Build()
+  public override dynamic Build()
   {
     var outPattern = _pattern.ToString();
 
@@ -97,10 +97,12 @@ public class GroupBuilder : Builder
     {
       _ = EndGroup();
       outPattern = _pattern.ToString();
+      if (!outPattern.EndsWith(")"))
+        throw new ArgumentException("The group is not closed.");
     }
-    _regexBuilder._pattern.Append(outPattern);
+    _builder._pattern.Append(outPattern);
     Validate();
-    return _regexBuilder;
+    return _builder;
   }
 
   /// <summary>
@@ -108,14 +110,23 @@ public class GroupBuilder : Builder
   /// </summary>
   public override CharacterClassBuilder StartCharacterClass()
   {
-    return new CharacterClassBuilder(_regexBuilder);
+    return new CharacterClassBuilder(this);
   }
 
   /// <summary>
   /// Starts building the group by adding an opening parenthesis to the pattern. Depending on the <see cref="GroupType"/>, the method will add the appropriate syntax to the pattern.
   /// </summary>
   /// <returns> A new instance of the <see cref="GroupBuilder"/> class. </returns>
-  public override GroupBuilder StartGroup()
+  // public GroupBuilder StartGroup()
+  // {
+  //   return new GroupBuilder(this._builder);
+  // }
+
+  /// <summary>
+  /// Starts building the group by adding an opening parenthesis to the pattern. Depending on the <see cref="GroupType"/>, the method will add the appropriate syntax to the pattern.
+  /// </summary>
+  /// <returns> A new instance of the <see cref="GroupBuilder"/> class. </returns>
+  internal GroupBuilder Start_Group()
   {
     _pattern.Append('(');
     switch (GroupType)
@@ -169,7 +180,7 @@ public class GroupBuilder : Builder
   /// <returns> A new instance of the <see cref="AnchorBuilder"/> class. </returns>
   public override AnchorBuilder StartAnchor()
   {
-    return new AnchorBuilder(_regexBuilder);
+    return new AnchorBuilder(this);
   }
 
   /// <summary>
