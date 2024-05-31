c10ac4557bb5276dcfc80f2d56edabbe9b309b13
Rob Bleattler <rbleattler@outlook.com>
2024-05-31 13:57:48 -0400

    refactor: Add InvalidCharacterEscapeException class for handling invalid character escape sequences

---
+++ src/FluentRegex/Classes/InvalidCharacterEscapeException.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FluentRegex.Exceptions;
+
+/// <summary>
+/// A custom exception thrown when an invalid character escape sequence is detected.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="InvalidCharacterEscapeException"/> class.
+/// </remarks>
+/// <param name="targetCharacter">The character that was attempted to be escaped.</param>
+/// <param name="pattern">The pattern that the character was found in.</param>
+/// <param name="indexOfCharacter">The index of the character in the pattern.</param>
+/// <param name="escapeSequence">The escape sequence that was used.</param>
+/// <exception cref="InvalidCharacterEscapeException">Thrown when an invalid character escape sequence is detected.</exception>
+[Serializable]
+public class InvalidCharacterEscapeException(char targetCharacter, string pattern, int indexOfCharacter, string escapeSequence) : Exception($"Invalid escape sequence '{escapeSequence}' for character '{targetCharacter}' at index {indexOfCharacter} in pattern '{pattern}' (pattern: {pattern} | index: {indexOfCharacter} | escape sequence: {escapeSequence} | target character: {targetCharacter})")
+{
+}
