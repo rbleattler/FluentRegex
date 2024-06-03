5b3e29b1e090e937e60753c3b1acba0f3fd215a7
Robert Bleattler <rob.bleattler@viewpointe.com>
2024-06-03 10:13:08 -0400

    refactor: Update LocalBuild.ps1 to include default version and verbose logging

--- Scripts/LocalBuild.ps1
+++ Scripts/LocalBuild.ps1
@@ -26,7 +26,7 @@ begin {
   function Invoke-LocalBuild {
     param(
       [string]
-      $Version,
+      $Version = '0.0.1',
       [switch]
       $Pack,
       [string]
@@ -34,30 +34,37 @@ begin {
       [List[string]]
       $Steps
     )
+    Write-Verbose "[Invoke-LocalBuild]:: Steps: $($Steps -join ', ')"
     switch ($Steps) {
       'Clean' {
         Write-Host "Cleaning project..." -ForegroundColor Yellow &&
+        Write-Verbose "[CLEAN]"
         dotnet clean
       }
       'Restore' {
         Write-Host "Restoring packages..." -ForegroundColor Yellow &&
+        Write-Verbose "[RESTORE]"
         dotnet restore
       }
       'Build' {
         Write-Host "Building version $Version..." -ForegroundColor Yellow &&
-        dotnet build
+        Write-Verbose "[BUILD]"
+        dotnet build --configuration $Configuration
       }
       'Test' {
         Write-Host "Running tests..." -ForegroundColor Yellow &&
+        Write-Verbose "[TEST]"
         dotnet test
       }
       'Publish' {
         Write-Host "Publishing..." -ForegroundColor Yellow &&
+        Write-Verbose "[PUBLISH]"
         dotnet publish $PSScriptRoot\..\src\FluentRegex\FluentRegex.csproj --configuration $Configuration /p:version="$Version" /p:PackageVersion="$Version" /p:InformationalVersion="$Version-local"
       }
       'Pack' {
         Write-Host "Packaging..." -ForegroundColor Yellow &&
-        dotnet pack $PSScriptRoot\..\src\FluentRegex\FluentRegex.csproj --output $OutputDirectory -p:NuspecFile=$PSScriptRoot\..\src\FluentRegex\FluentRegex.nuspec -p:version="$($Version)" /p:PackageVersion="$($Version)" /p:PackageReleaseNotes="Local build" /p:PackageTags="local"
+        Write-Verbose "[PACK]"
+        dotnet pack $PSScriptRoot\..\src\FluentRegex\FluentRegex.csproj --output $OutputDirectory -p:NuspecFile=$PSScriptRoot\..\src\FluentRegex\FluentRegex.nuspec -p:version="$($Version)" /p:PackageVersion="$($Version)" /p:PackageReleaseNotes="Local build" /p:PackageTags="local" /p:NugetVersion="$($Version)-local"
       }
       default {
         Write-Warning "I'm not sure what you're trying to do here... But we're going to assume it's 'Everything' and start over..."
@@ -65,12 +72,17 @@ begin {
       }
     }
   }
-
+  if ($Steps.Count -eq 0) {
+    Write-Warning "No steps were provided. Defaulting to 'Everything'..."
+    $PSBoundParameters["Steps"] = @( 'Clean', 'Restore', 'Build', 'Test', 'Publish', 'Pack' )
+    # $Steps = @( 'Clean', 'Restore', 'Build', 'Test', 'Publish','Pack' )
+  }
   Write-Debug "[Begin][LocalBuild.ps1][Exit]"
 }
 process {
   Write-Debug "[Process][LocalBuild.ps1][Enter]"
 
+  Write-Verbose "Steps: $($Steps -join ', ')"
   Invoke-LocalBuild @PSBoundParameters
 
   Write-Debug "[Process][LocalBuild.ps1][Exit]"
