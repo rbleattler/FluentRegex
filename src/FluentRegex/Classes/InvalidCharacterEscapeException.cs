70bc7c8b6f6f38b357a474fd267f57b1cb33e354
Rob Bleattler <rbleattler@outlook.com>
2024-06-28 11:19:05 -0400

    Remove unnecessary serializable attribute from InvalidCharacterEscapeException

--- src/FluentRegex/Classes/InvalidCharacterEscapeException.cs
+++ src/FluentRegex/Classes/InvalidCharacterEscapeException.cs
@@ -15,7 +15,7 @@ namespace FluentRegex.Exceptions;
 /// <param name="indexOfCharacter">The index of the character in the pattern.</param>
 /// <param name="escapeSequence">The escape sequence that was used.</param>
 /// <exception cref="InvalidCharacterEscapeException">Thrown when an invalid character escape sequence is detected.</exception>
-[Serializable]
+
 public class InvalidCharacterEscapeException(char targetCharacter, string pattern, int indexOfCharacter, string escapeSequence) : Exception($"Invalid escape sequence '{escapeSequence}' for character '{targetCharacter}' at index {indexOfCharacter} in pattern '{pattern}' (pattern: {pattern} | index: {indexOfCharacter} | escape sequence: {escapeSequence} | target character: {targetCharacter})")
 {
 }
